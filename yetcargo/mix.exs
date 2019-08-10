cd5c9b96d2c89da224547787c578e968d5a94f78
francisco.ramirez <francisco.ramirez@DSA-DEV2-09>
2019-08-10 10:23:58 -0500

    correccion en el mix

--- yetcargo/mix.exs
+++ yetcargo/mix.exs
@@ -20,7 +20,7 @@ defmodule Yetcargo.MixProject do
   def application do
     [
       mod: {Yetcargo.Application, []},
-      extra_applications: [:logger, :runtime_tools, :httpoison]
+      extra_applications: [:logger, :runtime_tools]
     ]
   end
 
